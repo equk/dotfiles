2472e35bbc247aaf532f25f147a6c16bd09b8aa0
equk <equk@users.noreply.github.com>
2021-09-18 12:05:24 +0100

    added some keymaps for basic lsp code navigation

--- configs/.config/nvim/lua/eq/keymap/init.lua
+++ configs/.config/nvim/lua/eq/keymap/init.lua
@@ -6,13 +6,18 @@ local key_mapper = function(mode, key, result)
   })
 end
 
--- telescope keybindings
+-- telescope
 key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files({previewer = false})<CR>')
 key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
 key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
 key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
 key_mapper('n', '<leader>gf', ':lua require"telescope.builtin".git_files({previewer = false})<CR>')
 key_mapper('n', '<leader>gs', ':lua require"telescope.builtin".git_status()<CR>')
+
+-- lsp
+key_mapper('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', '<c-]>', ':lua vim.lsp.buf.definition()<CR>')
+key_mapper('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
 
 -- move lines
 key_mapper('n', '<A-up>', ':m .-2<CR>')
