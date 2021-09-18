6eac98e0e9a6ab52805e0fecce49367b3134408f
equk <equk@users.noreply.github.com>
2021-09-18 12:06:12 +0100

    added tab completion to cmp

--- configs/.config/nvim/lua/eq/completion/init.lua
+++ configs/.config/nvim/lua/eq/completion/init.lua
@@ -14,6 +14,8 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
     },
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
   },
   sources = {
     { name = 'nvim_lsp' },
