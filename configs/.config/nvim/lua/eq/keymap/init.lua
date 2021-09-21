b30e3690856e6bd9e45502e7c7d58fa88e2de115
equk <equk@users.noreply.github.com>
2021-09-21 12:45:41 +0100

    cleaned up telescope keymaps for disabling previewer

--- configs/.config/nvim/lua/eq/keymap/init.lua
+++ configs/.config/nvim/lua/eq/keymap/init.lua
@@ -7,11 +7,11 @@ local key_mapper = function(mode, key, result)
 end
 
 -- telescope
-key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files({previewer = false})<CR>')
+key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files { previewer = false }<CR>')
 key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
 key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
 key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
-key_mapper('n', '<leader>gf', ':lua require"telescope.builtin".git_files({previewer = false})<CR>')
+key_mapper('n', '<leader>gf', ':lua require"telescope.builtin".git_files { previewer = false }<CR>')
 key_mapper('n', '<leader>gs', ':lua require"telescope.builtin".git_status()<CR>')
 
 -- lsp
@@ -28,3 +28,4 @@ key_mapper('n', '<A-up>', ':m .-2<CR>')
 key_mapper('n', '<A-down>', ':m .+1<CR>')
 key_mapper('n', '<A-k>', ':m .-2<CR>')
 key_mapper('n', '<A-j>', ':m .+1<CR>')
+
