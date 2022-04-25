c5eb89ace89ffa1433d024765d947efbda5b159f
equk <equk@users.noreply.github.com>
2022-04-25 15:47:34 +0100

    neovim: replaced deprecated ensure_installed=maintained in treesitter config

--- configs/.config/nvim/lua/eq/treesitter/init.lua
+++ configs/.config/nvim/lua/eq/treesitter/init.lua
@@ -1,7 +1,7 @@
 -- treesitter config
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = {'python', 'cpp', 'lua', 'rust'},
   highlight = {
     enable = true,
   },
