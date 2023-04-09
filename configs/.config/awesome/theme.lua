c6d81d36c4d1cebfbbb60b022589048427ecb43f
equk <equk@users.noreply.github.com>
2023-04-09 15:28:08 +0100

    awesomewm: changed window border to 1 pixel

--- configs/.config/awesome/theme.lua
+++ configs/.config/awesome/theme.lua
@@ -27,7 +27,7 @@ theme.fg_urgent = '#eb709b'
 theme.fg_minimize = '#ffffff'
 
 theme.useless_gap = dpi(0)
-theme.border_width = dpi(2)
+theme.border_width = dpi(1)
 theme.border_normal = '#000000'
 theme.border_focus = '#8655a8'
 theme.border_marked = '#565656'
