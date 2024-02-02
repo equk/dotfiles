77d56ae1681fa16804be95f3f40adbfabe1113c5
equk <equk@users.noreply.github.com>
2024-02-02 15:53:18 +0000

    neovim - added javascript & typescript to treesitter array

--- configs/.config/nvim/lua/eq/treesitter/init.lua
+++ configs/.config/nvim/lua/eq/treesitter/init.lua
@@ -1,7 +1,7 @@
 -- treesitter config
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
-  ensure_installed = { 'python', 'cpp', 'lua', 'rust' },
+  ensure_installed = { 'python', 'cpp', 'lua', 'rust', 'javascript', 'typescript' },
   highlight = {
     enable = true,
   },
