c28a5e7b636a0d992aefbee5c0633a0e6f2a5143
equk <equk@users.noreply.github.com>
2026-02-25 16:26:24 +0000

    neovim - add tmux navigation plugin

--- configs/.config/nvim/lua/eq/plugins/init.lua
+++ configs/.config/nvim/lua/eq/plugins/init.lua
@@ -127,6 +127,28 @@ return require('lazy').setup {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
   },
+  -- tmux navigation
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-Left>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-Down>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-Up>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-Right>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    },
+  },
   ---- linter on save
   'dense-analysis/ale',
   ---- display indentation lines
