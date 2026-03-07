db2a9214b18402d7895834f9470cad2932f995a8
equk <equk@users.noreply.github.com>
2026-03-07 11:02:09 +0000

    neovim - disable unused plugin providers

--- configs/.config/nvim/lua/eq/options/init.lua
+++ configs/.config/nvim/lua/eq/options/init.lua
@@ -50,3 +50,8 @@ vim.opt.list = true
 vim.opt.mouse = ''
 -- set clipboard for tmux
 vim.opt.clipboard = 'unnamedplus'
+-- disable unused plugin providers
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
