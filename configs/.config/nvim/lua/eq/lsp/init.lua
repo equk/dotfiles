b4d5a9dbd041c96ecd4c605c5692fbe5987a82df
equk <equk@users.noreply.github.com>
2026-03-08 12:29:46 +0000

    neovim - add eslint fix on save using BufWritePre autocmd

--- configs/.config/nvim/lua/eq/lsp/init.lua
+++ configs/.config/nvim/lua/eq/lsp/init.lua
@@ -3,6 +3,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 --- use neovim vim.lsp.config
 local lspconf = vim.lsp.config
+--- eslint on_attach
+local eslint_on_attach = vim.lsp.config.eslint.on_attach
 ---- lsp sources
 -- [lsp] rust-analyzer
 lspconf.rust_analyzer = {
@@ -42,6 +44,17 @@ lspconf.ts_ls = {
   capabilities = capabilities,
   flags = { debounce_text_changes = 400 },
 }
+-- [lsp] eslint
+vim.lsp.config("eslint", {
+  on_attach = function(client, bufnr)
+    if not eslint_on_attach then return end
+    eslint_on_attach(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "LspEslintFixAll",
+    })
+  end,
+})
 -- [lsp] vuejs
 lspconf.vue_ls = {
   capabilities = capabilities,
@@ -104,6 +117,7 @@ vim.lsp.enable {
   'rust_analyzer',
   'bacon_ls',
   'ts_ls',
+  'eslint',
   'vue_ls',
   'svelte',
   'gopls',
